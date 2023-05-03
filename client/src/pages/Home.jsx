8e2caaf3b4b3802399f89f16c0af0e189ee95043
Hansa-Hettiarachchi <hansahettiarachchi@gmail.com>
2023-05-03 11:22:15 +0530

    create frid

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,14 @@ const Home = () => {
 
   const [loading , setLoading] = useState(false);
   const [allPosts , setAllPosts] = useState(null);
-  const [searchText, setSearchText] = useState("sssS")
+  const [searchText, setSearchText] = useState("");
+
+  const RenderCards = ({data, title}) => {
+    if(data?.length > 0 ) {
+      return data.map((post) => <Card key={post._id} {...post} />)
+    }
+    return <h2 className='mt-5 text-[#6449ff] text-xl uppercase'>{title}</h2>
+  }
 
   return (
     <section className='max-w-7xl mx auto'>
@@ -28,15 +35,26 @@ const Home = () => {
           </div>
         ) : (
           <>
-          {searchText && (
-            <h2 className='font-meduim text-[#666e75] text-xl mb-3'>Showing results for <span className='text-[#222328]'>{searchText}</span>
-            </h2>
-          )}
-
+            {searchText && (
+              <h2 className='font-meduim text-[#666e75] text-xl mb-3'>Showing results for <span className='text-[#222328]'>{searchText}</span>
+              </h2>
+            )}
+            <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+              {searchText ? (
+                <RenderCards 
+                  data= {[]}
+                  title = "No result Found" 
+                  />
+              ) : (
+                <RenderCards
+                  data= {[]}
+                  title = "No Posts Found"
+                />
+              )}
+            </div>
           </>
         )}
       </div>
-
     </section>
   )
 }
